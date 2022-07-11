ea2054ea02bcc6db5a75177ecf01a6649f7ee6c9
Sianarnd <106249359+Sianarnd@users.noreply.github.com>
2022-07-11 07:11:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,15 +19,15 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94770342724']
+global.premium = ['+94770342724']
+global.ownernomer = '+94770342724'
+global.ownername = '➤ʀᴀᴡɪɴᴅᴜ.'
+global.botname = '➤ʀɴᴅ Bᴏᴛᴢ'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
+global.sc = 'https://github.com/Sianarnd/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
